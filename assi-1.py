d5f02c20565f2e532057df68aec62b47c6ba6ae1
Aman Kamal <109606739+KAMALAMAN687@users.noreply.github.com>
2022-12-11 19:49:16 +0530

    Add files via upload

---
+++ assi-1.py
@@ -0,0 +1,14 @@
+print("hello")
+print("pthon")
+print("\"MysirG\"")
+print("\"Teacher’s Day\"")
+a="aman kamal"
+print(a)
+name="Aman Kamal"
+age=20
+qualification="B.Tech"
+year_of_experience="none"
+print(name,age, qualification,year_of_experience,sep="@",end="\n")
+c=2
+d=6
+print(c,d,sep="\n")
